7818c2ae718922eaf5ec51b6c1fcb36d3a0237ae
sazzadi-r14 <sazzadi.cc9@gmail.com>
2023-04-09 16:52:26 -0700

    Assignment 0 Complete

--- assign0/public/survey.js
+++ assign0/public/survey.js
@@ -6,32 +6,32 @@
 /*** About You ***/
 
 /* The one you use to log in, not the number */
-export const sunetid = "TODO";
+export const sunetid = "sazzad14";
 
 /* How would you like us to refer to you? */
-export const name = "TODO";
+export const name = "Sazzad";
 
 /* Please share if you are comfortable doing so. You may leave this blank if you'd prefer. */
-export const pronouns = "TODO";
+export const pronouns = "he/him";
 
 /* E.g. frosh, sophomore, coterm, etc. */
-export const classYear = "TODO";
+export const classYear = "frosh";
 
 /* Major or department. If unsure/undeclared, what are you considering? */
-export const major = "TODO";
+export const major = "Computer Science";
 
 /*** About Your Experiences ***/
 
 /* What topic/part of the class are you most interested in? */
-export const topicInterest = "TODO";
+export const topicInterest = "Being able to build web applications";
 
 /* Do you have any prior experience with web development?
    If so, please describe it. If not, just remove the TODO. */
-export const priorWebdev = "TODO";
+export const priorWebdev = "";
 
 /* What's your favorite CS (or CS related) experience?
    E.g. a class you've taken or a project you worked on. */
-export const favCSExp = "TODO";
+export const favCSExp = "Autonomous Robotics";
 
 /* Anything else you want us to know? You can leave this blank by removing the TODO. */
-export const anythingElse = "TODO";
+export const anythingElse = "I love how increidbel Michael is in the class, I look forward to learning from him";
