53a3d3a7003faff06c84096c1e534e2ae7569462
sazzadi-r14 <sazzadi.cc9@gmail.com>
2023-04-09 15:40:45 -0700

    maiden commit

---
+++ assign0/public/survey.js
@@ -0,0 +1,37 @@
+/* Welcome to CS193X! We'd like to get to know you a bit and learn a bit about your experiences.
+
+   Please fill in your answers below. Make sure not to change the variable names or remove the `export`.
+   Since the values are all strings, they will need to be quoted. You can use backquotes (`) if you want to easily include a newline. */
+
+/*** About You ***/
+
+/* The one you use to log in, not the number */
+export const sunetid = "TODO";
+
+/* How would you like us to refer to you? */
+export const name = "TODO";
+
+/* Please share if you are comfortable doing so. You may leave this blank if you'd prefer. */
+export const pronouns = "TODO";
+
+/* E.g. frosh, sophomore, coterm, etc. */
+export const classYear = "TODO";
+
+/* Major or department. If unsure/undeclared, what are you considering? */
+export const major = "TODO";
+
+/*** About Your Experiences ***/
+
+/* What topic/part of the class are you most interested in? */
+export const topicInterest = "TODO";
+
+/* Do you have any prior experience with web development?
+   If so, please describe it. If not, just remove the TODO. */
+export const priorWebdev = "TODO";
+
+/* What's your favorite CS (or CS related) experience?
+   E.g. a class you've taken or a project you worked on. */
+export const favCSExp = "TODO";
+
+/* Anything else you want us to know? You can leave this blank by removing the TODO. */
+export const anythingElse = "TODO";
